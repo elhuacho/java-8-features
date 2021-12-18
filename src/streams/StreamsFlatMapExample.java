65b044b50d4b8a375791ecb5ac5b8b7c061ae934
Walter Huacho <walter1320081@hotmail.com>
2021-12-18 14:51:25 -0500

    sorted ejemplos

--- src/streams/StreamsFlatMapExample.java
+++ src/streams/StreamsFlatMapExample.java
@@ -15,11 +15,23 @@ public class StreamsFlatMapExample {
         List<String> studentActivities = StudentDataBase.getAllStudents().stream() //Stream<Student>
                 .map(Student::getActivities) // Stream<List<String>>
                 .flatMap(List::stream) // Stream<String>
+                .distinct()
+                .sorted()
                 .collect(toList());
         return studentActivities;
     }
 
+    public static long getStudentActivitiesCount() {
+         long numberOfStudentActivities = StudentDataBase.getAllStudents().stream() //Stream<Student>
+                .map(Student::getActivities) // Stream<List<String>>
+                .flatMap(List::stream) // Stream<String>
+                .distinct() // Stream<String> -> with distinct function perform
+                .count();
+        return numberOfStudentActivities;
+    }
+
     public static void main(String[] args) {
         System.out.println("printStudentActivities : " + printStudentActivities());
+        System.out.println("getStudentActivitiesCount : " + getStudentActivitiesCount());
     }
 }
