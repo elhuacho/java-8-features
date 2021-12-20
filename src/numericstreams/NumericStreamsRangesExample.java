85d9dcd9acfdc9ef11e4d3c9710e146a26e9ebc8
Walter Huacho <walter1320081@hotmail.com>
2021-12-20 16:49:42 -0500

    numeric streams

---
+++ src/numericstreams/NumericStreamsRangesExample.java
@@ -0,0 +1,25 @@
+package numericstreams;
+
+import java.util.stream.IntStream;
+import java.util.stream.LongStream;
+
+public class NumericStreamsRangesExample {
+    public static void main(String[] args) {
+        IntStream intStream = IntStream.range(1,50);
+        System.out.println("Range count : " + intStream.count());
+        IntStream.range(1,50).forEach((value -> System.out.print(value + ",")));
+        System.out.println();
+
+        System.out.println("Range closed count : " + IntStream.rangeClosed(1,50).count());
+        IntStream.rangeClosed(1,50).forEach((value -> System.out.print(value + ",")));
+        System.out.println();
+
+        System.out.println("Long Stream Range closed count : " + LongStream.rangeClosed(1,50).count());
+        LongStream.rangeClosed(1,50).forEach((value -> System.out.print(value + ",")));
+        System.out.println();
+
+        IntStream.rangeClosed(1,50).asDoubleStream().forEach((value -> System.out.print(value + ",")));
+
+
+    }
+}
