e204de7e875af3a67e5cc17097f13cded88b1c01
Walter Huacho <walter1320081@hotmail.com>
2021-12-20 16:12:03 -0500

    stream of iterate generate ejemplos

--- src/functionalinterfaces/SupplierExample.java
+++ src/functionalinterfaces/SupplierExample.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.function.Supplier;
 
-public class SuplierExample {
+public class SupplierExample {
     public static void main(String[] args) {
         Supplier<Student> studentSupplier = () -> {
             return new Student("Adam",2,3.6, "male",10, Arrays.asList("swimming", "basketball","volleyball"));
